5de8513afc06e21db5f357d2c63e7ee1b8447486
leenapark <hrn424@gmail.com>
2021-01-13 17:56:25 +0900

    20210113 Vo 추가

--- src/com/javaex/vo/UserVo.java
+++ src/com/javaex/vo/UserVo.java
@@ -22,6 +22,16 @@ public class UserVo {
 		this.name = name;
 	}
 
+	
+	// modify 생성자
+	public UserVo(int no, String password, String name, String gender) {
+		super();
+		this.no = no;
+		this.password = password;
+		this.name = name;
+		this.gender = gender;
+	}
+
 	//	insert 생성자
 	public UserVo(String id, String password, String name, String gender) {
 		this.id = id;
