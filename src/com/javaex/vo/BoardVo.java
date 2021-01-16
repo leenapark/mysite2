d1aab219246ecbfcfeaffcbdd5584311ae5f758e
leenapark <hrn424@gmail.com>
2021-01-17 03:03:50 +0900

    20210115 BoardVo 생성

--- src/com/javaex/vo/BoardVo.java
+++ src/com/javaex/vo/BoardVo.java
@@ -10,24 +10,84 @@ public class BoardVo {
 	private int hit;
 	private String regDate;
 	private int userNo;
-	
+	private String id;
+	private String name;
 	
 	// 생성자	
 	public BoardVo() {
 	}
 
+	/*******modify 생성자*****/
+	public BoardVo(int no, String title, String content) {
+		this.no = no;
+		this.title = title;
+		this.content = content;
+	}
 
-	public BoardVo(int no, String title, String content, int hit, String regDate, int userNo) {
+	/*****write 생성자*****/
+	public BoardVo(String title, String content, int userNo) {
+		super();
+		this.title = title;
+		this.content = content;
+		this.userNo = userNo;
+	}
+	
+	
+	
+
+
+	/**********read test*********/
+	public BoardVo(String title, String content, int hit, String regDate, String id, String name) {
+		super();
+		this.title = title;
+		this.content = content;
+		this.hit = hit;
+		this.regDate = regDate;
+		this.id = id;
+		this.name = name;
+	}
+
+	
+	
+	
+	public BoardVo(String title, String content, int hit, String regDate, int userNo, String id, String name) {
+		this.title = title;
+		this.content = content;
+		this.hit = hit;
+		this.regDate = regDate;
+		this.userNo = userNo;
+		this.id = id;
+		this.name = name;
+	}
+
+	public BoardVo(int no, String title, String content, int hit, String regDate, int userNo, String name) {
+		super();
 		this.no = no;
 		this.title = title;
 		this.content = content;
 		this.hit = hit;
 		this.regDate = regDate;
 		this.userNo = userNo;
+		this.name = name;
+	}
+
+	
+	
+	public BoardVo(int no, String title, String content, int hit, String regDate, int userNo, String id, String name) {
+		this.no = no;
+		this.title = title;
+		this.content = content;
+		this.hit = hit;
+		this.regDate = regDate;
+		this.userNo = userNo;
+		this.id = id;
+		this.name = name;
 	}
 	
 	
+	
 	// 메소드 g/s
+
 
 	public int getNo() {
 		return no;
@@ -88,15 +148,32 @@ public class BoardVo {
 		this.userNo = userNo;
 	}
 
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
 	
 	// 메소드 일반
 
-	
-	// test
+
 	@Override
 	public String toString() {
 		return "BoardVo [no=" + no + ", title=" + title + ", content=" + content + ", hit=" + hit + ", regDate="
-				+ regDate + ", userNo=" + userNo + "]";
+				+ regDate + ", userNo=" + userNo + ", id=" + id + ", name=" + name + "]";
 	}
 	
 
